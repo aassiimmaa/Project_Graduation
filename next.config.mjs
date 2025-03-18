f194d3588eb181ecb5d105bfe7a164f6e8aeaa12
aassiimmaa <nguyenthanhanvp2690@gmail.com>
2025-03-18 17:04:45 +0700

    adjust navbar, slider, complete UI Services

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'icdn.24h.com.vn',
+          pathname: '/upload/**',
+        },
+      ],
+    },
+  };
+  
+  export default nextConfig;
+  
---
+++ src/app/components/CategoriesList.tsx
@@ -0,0 +1,49 @@
+import { Box, Container, Typography } from '@mui/material'
+import React from 'react'
+import CategoryItem from './CategoryItem'
+
+const CategoriesList = () => {
+  //Style Section
+  const styleLayoutService = {
+    paddingY: '60px'
+  }
+
+  const styleTitle = {
+    color: '#000',
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: '48px'
+  }
+
+  return (
+    <Container maxWidth="xl" sx={styleLayoutService}>
+      <Typography sx={styleTitle} variant="h4">
+        Chọn dòng xe cần phục vụ
+      </Typography>
+      <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+        <CategoryItem
+          image="https://icdn.24h.com.vn/upload/2-2023/images/2023-04-28/image7-1682662731-746-width1200height793.jpg"
+          name="Xe SUV"
+          description="Thoáng mát, rộng rãi cho các chuyến đi đường dài và hành trình nhóm."
+        />
+        <CategoryItem
+          image="https://icdn.24h.com.vn/upload/2-2023/images/2023-04-28/image7-1682662731-746-width1200height793.jpg"
+          name="Xe SUV"
+          description="Thoáng mát, rộng rãi cho các chuyến đi đường dài và hành trình nhóm."
+        />
+        <CategoryItem
+          image="https://icdn.24h.com.vn/upload/2-2023/images/2023-04-28/image7-1682662731-746-width1200height793.jpg"
+          name="Xe SUV"
+          description="Thoáng mát, rộng rãi cho các chuyến đi đường dài và hành trình nhóm."
+        />
+        <CategoryItem
+          image="https://icdn.24h.com.vn/upload/2-2023/images/2023-04-28/image7-1682662731-746-width1200height793.jpg"
+          name="Xe SUV"
+          description="Thoáng mát, rộng rãi cho các chuyến đi đường dài và hành trình nhóm."
+        />
+      </Box>
+    </Container>
+  )
+}
+
+export default CategoriesList
